334026dea4b69e6d252a1bcd3f9ce98dda6fac3b
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-11-09 00:36:03 +0700

    Update HttpClient.js

--- helpers/HttpClient.js
+++ helpers/HttpClient.js
@@ -4,8 +4,7 @@ const Fetcheader = async (url) => {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/83.0',
             'Origin': url,
-            'Referer': url,
-            'Accept-Language: en-US,en;q=0.9'
+            'Referer': url
         }
     });
 
