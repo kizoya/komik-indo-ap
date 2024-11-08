27bfbcc9410c2267c12971a4c7d0784498201a3a
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-11-09 00:42:14 +0700

    Update HttpClient.js

--- helpers/HttpClient.js
+++ helpers/HttpClient.js
@@ -4,7 +4,10 @@ const Fetcheader = async (url) => {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/83.0',
             'Origin': url,
-            'Referer': url
+            'Referer': url,
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',  // Menentukan tipe konten yang diterima
+            'Accept-Encoding': 'gzip, deflate, br, zstd',  // Menentukan jenis encoding yang diterima
+            'Accept-Language': 'id-ID'
         }
     });
 
