a1addf579bc13bb823b4d81a25c67fd2031b227d
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-11-09 00:32:46 +0700

    Update HttpClient.js

--- helpers/HttpClient.js
+++ helpers/HttpClient.js
@@ -3,8 +3,8 @@ const Fetcheader = async (url) => {
     const { data } = await axios.get(url, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/83.0',
-            'Origin': url,
-            'Referer': url
+            'Referer: https://komikindo.biz/',
+            'Accept-Language: en-US,en;q=0.9'
         }
     });
 
